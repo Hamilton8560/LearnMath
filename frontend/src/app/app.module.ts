7c5d15d793f8ddce07179ad73c38fde57a25ccdc
David Hamilton <davidhamilton473@gmail.com>
2023-11-09 14:33:19 -0500

    Example

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ButtonModule } from 'primeng/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    ButtonModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+constructor(private router:Router){}
+
+  onClick(){
+    this.router.navigate(['google.com'])
+  }
+
+}
